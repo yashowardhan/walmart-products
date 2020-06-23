d04932dfd2822f991a0e5dffc84db9e2926a9fbb
yashowardhan <yashkaushikbits19@gmail.com>
2020-06-23 14:42:12 +0530

    Update reducer.js

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,6 +1,7 @@
 import _ from "lodash";
+import { sortProducts } from "./fetchProducts";
 
-export default (state = {}, action) => {
+export const productsReducer = (state = {}, action) => {
   switch (action.type) {
     case "FETCH_PRODUCTS":
       return {
@@ -9,6 +10,18 @@ export default (state = {}, action) => {
       };
     case "FETCH_PRODUCT_DETAIL":
       return { ...state, [action.payload.productId]: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const sortReducer = (state = {}, action) => {
+  switch (action.type) {
+    case "SORT_PRODUCTS":
+      return {
+        ...state,
+        products: action.payload,
+      };
     default:
       return state;
   }
